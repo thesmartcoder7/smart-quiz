79c45c4045016ef9044aaec2057ca2b8df2dba72
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-02-05 09:06:30 +0300

    test: data collection -> testing whether form data is being collected

--- js/script.js
+++ js/script.js
@@ -7,6 +7,19 @@ var scoreRemark = document.querySelector(".remark");
 
 //form submission function to prevent the default redirection
 questionForm.addEventListener("submit", (e) => {
-    // alert("form has been submitted!");
-    e.preventDefault();
+    e.preventDefault(); //prevents browser redirection
+
+    let userResponses = [
+        questionForm.one.value,
+        questionForm.two.value,
+        questionForm.three.value,
+        questionForm.four.value,
+        questionForm.five.value,
+        questionForm.six.value,
+    ];
+
+    //testing if the user responses are being recorded
+    for (response in userResponses) {
+        console.log(userResponses[response]);
+    }
 });
