271b5a5ce12d72c5f5c52086bfb76956ee6a34d5
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2022-10-17 13:07:07 +0300

    Refactor: Add typescript to the codebase

---
+++ js/script.ts
@@ -0,0 +1,59 @@
+// declaring variables
+const correctAnswers = ["c", "b", "a", "b", "d", "c"];
+const questionForm = document.querySelector(".quiz-form") as HTMLFormElement;
+const scoreTag = document.querySelector(".result-display") as HTMLDivElement;
+const reloadBtn = document.querySelector(".reload") as HTMLButtonElement;
+var totalScore = document.querySelector(".score") as HTMLSpanElement;
+var scoreRemark = document.querySelector(".remark") as HTMLSpanElement;
+var score = 0;
+
+//form submission function to prevent the default redirection
+if (questionForm) {
+  questionForm.addEventListener("submit", (e) => {
+    e.preventDefault(); //prevents browser redirection
+
+    let userResponses = [
+      questionForm.one.value,
+      questionForm.two.value,
+      questionForm.three.value,
+      questionForm.four.value,
+      questionForm.five.value,
+      questionForm.six.value,
+    ];
+
+    // testing the score increment for every right answer functionality
+    for (let item in userResponses) {
+      if (userResponses[item] === correctAnswers[item]) {
+        score++;
+      }
+    }
+    let percentageScore: number = Number(
+      ((score / correctAnswers.length) * 100).toFixed(2)
+    ); // converts this string to Number since the .toFixed function converts the numbers to strings
+
+    scoreTag.style.display = "flex";
+    reloadBtn.style.display = "inline";
+    totalScore.innerText = String(percentageScore);
+
+    if (percentageScore > 80) {
+      scoreRemark.innerText = "Impresively Done!";
+    } else if (percentageScore >= 50 && percentageScore <= 80) {
+      scoreRemark.innerText = "Fairly Done!";
+    } else {
+      scoreRemark.innerText = "Poorly Done!";
+    }
+
+    scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    questionForm.reset(); //resets the form
+  });
+}
+
+if (reloadBtn) {
+  reloadBtn.addEventListener("click", () => {
+    window.location.reload(); // reloads the page for a new try once the user clicks
+  });
+}
